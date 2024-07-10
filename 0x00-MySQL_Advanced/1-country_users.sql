8f6a0a7fa7d661c5540e33013a8d02d749f439cc
francotaind <francotaind00@gmail.com>
2024-07-10 14:31:42 +0300

    adding the sql zip file the best way i know how

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+--create a table called users if it doesnt exist
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+);
