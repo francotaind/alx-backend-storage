a3ca64539757890169649593c0ad67eb4a390546
francotaind <francotaind00@gmail.com>
2024-07-14 05:40:46 +0300

    tasks not complete

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,5 @@
---select the origin (country) and sum of fans grouped by origin
--- the number of (non-unique) fans
--- Column names must be: origin and nb_fans
--- Your script can be executed on any database
+--Ranks country origin of bands, ordered by number 0f (non-unique) fans in
+--descending order
 SELECT origin, SUM(fans) AS nb_fans
     FROM metal_bands
     GROUP BY origin
